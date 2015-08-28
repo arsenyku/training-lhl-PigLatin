056e3d0c83de7501bd66ed7b5b88884ba37f7e46
asu <arsenyk.ustaris.dev@gmail.com>
2015-08-28 12:34:29 -0700

    Initial Commit

---
+++ Pig Latin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pig Latin
+//
+//  Created by asu on 2015-08-28.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
